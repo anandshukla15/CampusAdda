0b47eff0ec42b5c78e1d13b1f310a49d51340d76
anandshukla15 <anandshukla1742@gmail.com>
2026-03-24 02:14:28 +0530

    server running

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("Hello World working fine");
 })
 
 const PORT = process.env.PORT || 5000;
