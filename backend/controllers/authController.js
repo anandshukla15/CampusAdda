f1951bdfe8657e9eb06e36bed4d3f47705d178ed
anandshukla15 <anandshukla1742@gmail.com>
2026-03-27 00:09:19 +0530

    register done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,3 +2,14 @@ const db=require("../config/db");
 const bcrypt=require("bcrypt");
 const jwt=require("jsonwebtoken");
 
+exports.register=async(req,res)=>{
+    const {name,email,password}=req.body;
+    const hashed=await bcrypt.hash(password,10);
+    db.query("INSERT INTO users(name,email,password) VALUES(?,?,?)",[name,email,hashed],(err,result)=>{
+        if(err){
+            res.status(500).json({error:err.message});
+        }else{
+            res.status(201).json({message:"User registered successfully"});
+        }
+    });
+};
