8c762c1b465ba27275d4ca71dcb19e6cb179325c
anandshukla15 <anandshukla1742@gmail.com>
2026-03-28 22:32:48 +0530

    controller part

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,3 +13,23 @@ exports.register=async(req,res)=>{
         }
     });
 };
+
+exports.login=async(req,res)=>{
+     const { email, password } = req.body;
+
+  db.query("SELECT * FROM users WHERE email=?", [email], async (err, result) => {
+    if (result.length === 0) return res.status(400).json({ msg: "User not found" });
+
+    const user = result[0];
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ msg: "Wrong password" });
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET
+    );
+
+    res.json({ token });
+  });
+};
