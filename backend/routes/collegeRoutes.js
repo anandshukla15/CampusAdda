cd10aa1f8ac2b1b157532d6a1366927d421bcfbd
anandshukla15 <anandshukla1742@gmail.com>
2026-04-01 20:42:32 +0530

    college routes

---
+++ backend/routes/collegeRoutes.js
@@ -0,0 +1,8 @@
+// const express = require('express');
+// const router = express.Router();
+// const { addCollege, getColleges } = require('../controllers/collegeController');
+
+// router.post('/add', addCollege);
+// router.get('/', getColleges);
+
+// module.exports = router;
