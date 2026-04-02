b2630e875718a9830e72f25df9a6eaff9aa38822
anandshukla15 <anandshukla1742@gmail.com>
2026-04-02 12:15:25 +0530

    server updated with college routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");   
 const authRoutes = require("./routes/authRoutes");
+const collegeRoutes = require("./routes/collegeRoutes");
 
 require("./config/db");
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/colleges", collegeRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Hello World working fine");
