04a69e98710259cff92d1820c358a2bb111c3e44
anandshukla15 <anandshukla1742@gmail.com>
2026-04-05 21:55:55 +0530

    controller done

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { request } = require('http');
+//const { request } = require('http');
 const multer = require('multer');
 const path = require('path');
 
 const router = express.Router();
 const upload = require('../middleware/upload');
 const { applyPresident ,approvePresident} = require('../controllers/presidentController');
+router.post('/apply', upload.single('document'), applyPresident);
+router.post('/approve/:userId', approvePresident);
+
+module.exports = router;
