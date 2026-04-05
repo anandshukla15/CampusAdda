513864ff8e5015e98758b537e8b890bf737a3a84
anandshukla15 <anandshukla1742@gmail.com>
2026-04-06 00:35:06 +0530

    sever updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");   
 const authRoutes = require("./routes/authRoutes");
 const collegeRoutes = require("./routes/collegeRoutes");
+const presidentRoutes = require("./routes/presidentRoutes");
 
 require("./config/db");
 const app = express();
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/colleges", collegeRoutes);
+app.use("/api/president", presidentRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Hello World working fine");
