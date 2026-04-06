3566db130040f3c8f6b3e9df192bd17a71328eeb
anandshukla15 <anandshukla1742@gmail.com>
2026-04-07 00:07:37 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/colleges", collegeRoutes);
-app.use("/api/president", presidentRoutes);
+app.use("/api/president",   presidentRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Hello World working fine");
