1bdbae84bb3886446bf1133331130d258f4b90c9
anandshukla15 <anandshukla1742@gmail.com>
2026-04-12 21:25:13 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const eventRoutes = require("./routes/eventRoutes");
 
 require("./config/db");
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
