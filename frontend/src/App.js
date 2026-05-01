73d4d742fc0d981e8bbfdf905d0385fe74fd140a
anandshukla15 <anandshukla1742@gmail.com>
2026-05-02 00:52:42 +0530

    app.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
-import PresidentDashboard from "./pages/PresidentDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
+import PresidentDashboard from "./pages/PresidentDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -12,11 +11,16 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/president" element={<PresidentDashboard />} />
+
         <Route path="/admin" element={
           <ProtectedRoute role="admin">
             <AdminDashboard />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/president" element={
+          <ProtectedRoute role="president">
+            <PresidentDashboard />
           </ProtectedRoute>
         } />
       </Routes>
