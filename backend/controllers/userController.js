18da63c9f1080ca27ab659ccf90c8d8c37cb47e4
anandshukla15 <anandshukla1742@gmail.com>
2026-06-11 00:12:03 +0530

    changes in contrikker

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,19 @@
 const db = require("../config/db");
 
 exports.getProfile = (req, res) => {
+  console.log("User ID from token:", req.user);
   const { id, role } = req.user;
   if (id === "admin") {
     return res.json({ id: "admin", name: "Admin", email: process.env.ADMIN_USERNAME, role: "admin" });
   }
 
   db.query(
-    "SELECT id,name,email,role,college_id,is_verified,document_url FROM users WHERE id=?",
+    "SELECT id,name,email,role,college_name,created_at FROM users WHERE id=?",
     [id],
     (err, result) => {
+      console.log("Profile query result:", err);
       if (err) return res.status(500).json(err);
+      
       if (!result || result.length === 0) return res.status(404).json({ message: "User not found" });
       res.json(result[0]);
     }
