82810e0737391498c87f392557926d25a88f85ff
anandshukla15 <anandshukla1742@gmail.com>
2026-06-17 00:04:13 +0530

    working model

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
+const upload = require("../middleware/upload");
 
-
-router.post("/register", register);
+router.post("/register", upload.single("document"), register);
 router.post("/login", login);
 
 module.exports = router;
