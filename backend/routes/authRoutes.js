9be27188bd3ee8990bdbb71d5401ad3f7ad9e645
anandshukla15 <anandshukla1742@gmail.com>
2026-06-18 00:22:24 +0530

    one route added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,19 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
 const upload = require("../middleware/upload");
+const jwt=require("jsonwebtoken");
+const passport=require("passport");
 
 router.post("/register", upload.single("document"), register);
 router.post("/login", login);
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+
+
 
 module.exports = router;
