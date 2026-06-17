f917b3d3b52a47389130eabdec19985860419552
anandshukla15 <anandshukla1742@gmail.com>
2026-06-18 00:28:12 +0530

    updated both routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -14,7 +14,26 @@ router.get(
   })
 );
 
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    session: false,
+  }),
+  (req, res) => {
+    const token = jwt.sign(
+      {
+        id: req.user.id,
+        role: req.user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
 
+    res.redirect(
+      `http://localhost:3000/oauth-success?token=${token}`
+    );
+  }
+);
 
 
 module.exports = router;
