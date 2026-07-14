8ea3ce89705bfe59a846b9bfceaf8c3611988224
anandshukla15 <anandshukla1742@gmail.com>
2026-07-15 00:28:09 +0530

    final update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({  
-    baseURL: 'https://campusadda-api.onrender.com/api'});
+    baseURL: 'https://campusadda-8e15.onrender.com/api'});
 
 
     API.interceptors.request.use((config) => {
